7a517f39a6d824a23b0cf8dd0da876170c331d60
Bethegnt <tyagigagan30@gmail.com>
2023-10-28 13:52:57 +0530

    start Document Object Model(Dom)

---
+++ dom/script.js
@@ -0,0 +1,29 @@
+// window => is object that contain everything{global javascript pbjects,functions,variables}.
+
+/* DOM : 
+window => document => html => head and body => and then sub part of attributes or elements.
+*/
+
+
+/* contain or seeing the content :
+1. innerHTML  => shows inner html in code.
+2. textContent => all the content show whether is hidden with styling or not.
+3. innerText => show only that visible content
+*/
+
+/* Select Elements:
+1. querySelector
+2. querySelectoraAll
+3. getElementById
+4. getElementByClassName
+5. getElementByName
+6. getAnimations
+*/
+
+/* there are two options :
+1. Nodelist
+2. HTMLCollection
+
+if u want to use forloops or other array properties then u have to apply this : Array.from().
+nodelist or HTMLCollection inside the ().
+*/
