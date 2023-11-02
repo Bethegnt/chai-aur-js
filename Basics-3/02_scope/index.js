31edd758a1f21df875c46b35d70e09ec6726e02c
Bethegnt <tyagigagan30@gmail.com>
2023-11-02 23:22:02 +0530

    scope done

---
+++ Basics-3/02_scope/index.js
@@ -0,0 +1,54 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    console.log("INNER: ", a);
+    
+}
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+function one(){
+    const username = "Gagan"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    console.log(website);
+
+     two()
+
+}
+one()
+
+if (true) {
+    const username = "Gagan"
+    if (username === "hiGagan") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+    console.log(website);
+}
+
+console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
