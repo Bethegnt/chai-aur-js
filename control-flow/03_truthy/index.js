82a2cbd7d50760516e7910103b75c8e9d2bb67f1
Bethegnt <tyagigagan30@gmail.com>
2023-11-03 11:35:21 +0530

    control-flow done

--- control-flow/03_truthy/index.js
+++ control-flow/03_truthy/index.js
@@ -1,4 +1,4 @@
-const userEmail = []
+const userEmail = "hi"
 
 if (userEmail) {
     console.log("Got user email");
@@ -13,10 +13,12 @@ if (userEmail) {
 //truthy values
 // "0", 'false', " ", [], {}, function(){}
 
-// if (userEmail.length === 0) {
-//     console.log("Array is empty");
-// }
+if (userEmail.length === 0) {
+    console.log("Array is empty");
+}
 
+
+// when u checked obj value truthy or falsy then u should change obj in to array.
 const emptyObj = {}
 
 if (Object.keys(emptyObj).length === 0) {
@@ -26,13 +28,10 @@ if (Object.keys(emptyObj).length === 0) {
 // Nullish Coalescing Operator (??): null undefined
 
 let val1;
-// val1 = 5 ?? 10
-// val1 = null ?? 10
-// val1 = undefined ?? 15
+val1 = 5 ?? 10
+val1 = null ?? 10
+val1 = undefined ?? 15
 val1 = null ?? 10 ?? 20
-
-
-
 console.log(val1);
 
 // Terniary Operator
