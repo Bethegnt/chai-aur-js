7c2a7b5e675ba27829649ff8c52d5a7acaa2835e
Bethegnt <tyagigagan30@gmail.com>
2023-11-03 11:25:43 +0530

    control-flow done

--- Basics-3/04_iife/index.js
+++ Basics-3/04_iife/index.js
@@ -1,5 +1,8 @@
-// Immediately Invoked Function Expressions (IIFE)
-
+/* Immediately Invoked Function Expressions (IIFE):-
+This helps prevent polluting the global namespace with variables and functions. 
+Anything defined inside the IIFE remains local to it, and it doesn't interfere with other scripts or libraries. 
+This is particularly important when working in larger codebases or collaborating with others to avoid naming conflicts.
+*/
 
 (function chai(){
     // named IIFE
@@ -9,3 +12,5 @@
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
 } )('gagan')
+
+// first () is the function's defination where we write fn and secound one () is the excection of the first () so its basically ()()
