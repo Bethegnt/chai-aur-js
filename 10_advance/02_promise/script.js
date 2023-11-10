c4511a3e596b568034e041513950ccfb97d4913c
Bethegnt <tyagigagan30@gmail.com>
2023-11-10 14:17:34 +0530

    promise done

--- 10_advance/02_promise/script.js
+++ 10_advance/02_promise/script.js
@@ -50,4 +50,49 @@ promiseFour.then((user)=>{
     console.log(error);
 }).finally(()=>console.log('the promise is either resolved or rejected.'))
 
+//+++++++++ Fifth promise ++++++++++
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = true;
+        if(!error){
+            resolve({username:'js',password:'123'})
+        }else{
+            reject('ERROR js went wrong')
+        }
+    },1000)
+})
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
+// yes this is also available, kuch reading aap b kro.
