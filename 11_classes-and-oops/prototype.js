5c7c24a2d3c7451ef686b72b99ff80693b4f688a
Bethegnt <tyagigagan30@gmail.com>
2023-11-18 22:32:04 +0530

    prototype done

---
+++ 11_classes-and-oops/prototype.js
@@ -0,0 +1,67 @@
+// let myName = "Gagan     "
+// let mychannel = "chai     "
+
+// console.log(myName.trueLength);
+
+
+let myHeros = ["thor", "spiderman"]
+
+
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+
+    getSpiderPower: function(){
+        console.log(`Spidy power is ${this.spiderman}`);
+    }
+}
+
+Object.prototype.Gagan = function(){
+    console.log(`Gagan is present in all objects`);
+}
+
+Array.prototype.heyGagan = function(){
+    console.log(`Gagan says hello`);
+}
+
+// heroPower.Gagan()
+// myHeros.Gagan()
+// myHeros.heyGagan()
+// heroPower.heyGagan()
+
+// inheritance
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "ChaiAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"Gagan".trueLength()
+"iceTea".trueLength()
