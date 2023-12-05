e93f7fdda7a26ec57339cf99d66f3d07a576babf
Bethegnt <tyagigagan30@gmail.com>
2023-12-05 16:39:33 +0530

    getter-setter done

---
+++ 11_classes-and-oops/getter_setter.js
@@ -0,0 +1,20 @@
+class User{
+    constructor(email,password){
+        this.email = email;
+        this.password = password;
+    }
+    get email(){
+        return this._email.toUpperCase()
+    }
+    set email(value){
+        this._email = value;
+    }
+    get password(){
+        return `${this._password}Gagan`
+    }
+    set password(value){
+        this._password = value
+    }
+}
+const Gagan = new User("Gagan@something.ai","abc")
+console.log(Gagan.email)
