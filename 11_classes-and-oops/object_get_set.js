b65789b8fe2fa0ac1e473fd8accd13df343166fb
Bethegnt <tyagigagan30@gmail.com>
2023-12-05 17:03:31 +0530

    object-get-set done

---
+++ 11_classes-and-oops/object_get_set.js
@@ -0,0 +1,16 @@
+const User = {
+    _email: 'abc@.com',
+    _password: "abc",
+
+
+    get email(){
+        return this._email.toUpperCase()
+    },
+
+    set email(value){
+        this._email = value
+    }
+}
+
+const tea = Object.create(User)
+console.log(tea.email);
